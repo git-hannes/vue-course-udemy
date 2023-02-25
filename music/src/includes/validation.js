57267d98676735f8f419137f8c21572c85f68c2f
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-02-25 13:15:23 +0100

    #108 done

--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -1,8 +1,11 @@
-import { Form as VeeForm, Field as VeeField } from 'vee-validate'
+import { Form as VeeForm, Field as VeeField, defineRule } from 'vee-validate'
+import { required, email, min, max, confirmed, length } from '@vee-validate/rules'
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm)
     app.component('VeeField', VeeField)
+
+    defineRule('required', required)
   }
 }
