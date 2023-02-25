05eba48e4e919cd0c95bddaba20343e9878ca254
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-02-25 17:13:39 +0100

    #116 done (side project SLOTS)

---
+++ side-projects/slot-props/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <hello-world #default="v">
+      <p>Hello {{ v.user.name }}</p>
+      <p>My favorite things are:</p>
+      <ul>
+        <li v-for="favorite in v.favorites" :key="favorite">
+          {{ favorite }}
+        </li>
+      </ul>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
